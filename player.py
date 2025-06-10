c1334325fb04cf4cec49e77ce92746acbeeaf22c
Juan David Merchan Torres <juan1014166@gmail.com>
2025-06-10 17:54:21 -0500

    feat: Add forward and backward movement

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_SPEED
 
 
 class Player(CircleShape):
@@ -25,6 +25,10 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_RADIUS * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -32,7 +36,7 @@ class Player(CircleShape):
             self.rotate(dt - 0.22)
         if keys[pygame.K_d]:
             self.rotate(dt + 0.22)
-        if keys[pygame.K_s]:
-            self.rotate(dt)
         if keys[pygame.K_w]:
-            self.rotate(dt)
+            self.move(dt + 0.016)
+        if keys[pygame.K_s]:
+            self.move(-dt)
