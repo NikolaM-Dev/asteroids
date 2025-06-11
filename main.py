c8d6c5d1d6d165c55847d6b1c0842061ebf48763
Juan David Merchan Torres <juan1014166@gmail.com>
2025-06-10 21:11:54 -0500

    feat: Add base asteroids spawn and animation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 
 from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from player import Player
 
 
@@ -10,13 +12,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0  # Delta Time in seconds
 
-    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Player.containers = (updatable, drawable)
+
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    Player.containers = (updatable, drawable)
-    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
